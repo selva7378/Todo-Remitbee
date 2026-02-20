a7feb90ba26661da05a5a0f14d495ab6fdd0860f
selva7378 <selvaganesh7378@gmail.com>
2026-02-20 15:15:42 +0530

    added Room database, restructured project packages, and created initial presentation components

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt.android.gradle.plugin) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.room) apply false
 }
